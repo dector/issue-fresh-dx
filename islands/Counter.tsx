0cd7bf5e2a7f25387083e6a134fa430a430bde25
Denys M <dector@dector.space>
2022-09-19 20:29:22 +0200

    Demo for denoland/fresh#784 issue

---
+++ islands/Counter.tsx
@@ -0,0 +1,20 @@
+import { useState } from "preact/hooks";
+import { Button } from "../components/Button.tsx";
+
+interface CounterProps {
+  start: number;
+}
+
+export default function Counter(props: CounterProps) {
+  const [count, setCount] = useState(props.start);
+
+  const wrongFunction = (p) => { await p(); }
+
+  return (
+    <div>
+      <p>{count}</p>
+      <Button onClick={() => setCount(count - 1)}>-1</Button>
+      <Button onClick={() => setCount(count + 1)}>+1</Button>
+    </div>
+  );
+}
